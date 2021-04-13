16c8f0dc4be58f1ecda7204b4233b9b8c9341006
Arthur Coelho <arthur.cavalcante.puc@gmail.com>
2021-04-13 02:58:32 -0300

    fix: remove main method from MySignature

--- MySignature.java
+++ MySignature.java
@@ -94,45 +94,4 @@ public class MySignature {
 
         return Hex(currentDigest).compareTo(Hex(this.originalDigest)) == 0;
     }
-
-    public static void main (String[] args) throws Exception {
-         //
-        // verifica args e recebe o texto plano
-        if (args.length !=1) {
-            System.err.println("Usage: java DigitalSignatureExample text");
-            System.exit(1);
-        }
-        byte[] plainText = args[0].getBytes("UTF8");
-        //
-        // gera o par de chaves RSA
-        System.out.println( "\nStart generating RSA key" );
-        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-        keyGen.initialize(2048);
-        KeyPair key = keyGen.generateKeyPair();
-        System.out.println( "Finish generating RSA key" );
-        //
-        // define um objeto signature para utilizar SHA1 e RSA
-        // e assina o texto plano com a chave privada,
-        // o provider utilizado tambem eh impresso
-        MySignature sig = MySignature.getInstance("MD5withRSA");
-        sig.initSign(key.getPrivate());
-        sig.update(plainText);
-        byte[] signature = sig.sign();
-//        System.out.println( sig.getProvider().getInfo() );
-        System.out.println( "\nSignature:" );
-        System.out.println(Hex(signature));
-
-        //
-        // verifica a assinatura com a chave publica
-        System.out.println( "\nStart signature verification" );
-        sig.initVerify(key.getPublic());
-        sig.update(plainText);
-        try {
-            if (sig.verify(signature)) {
-                System.out.println( "Signature verified" );
-            } else System.out.println( "Signature failed" );
-        } catch (Exception se) {
-            System.out.println( "Signature failed: " + se );
-        }
-    }
 }
